68ffb936ae206135321f1f0ee645c0f1f164f36c
Nguyễn Công Trình <nctrinh2705@gmail.com>
2024-04-05 21:06:29 +0700

    Submit HW!

---
+++ HW1/info.java
@@ -0,0 +1,14 @@
+public class info{
+    public static void main(String[] args){
+        System.out.print("Họ và tên: Nguyễn Công Trình\t");
+        System.out.print("MSV: 23020441\t");
+        System.out.print("Lớp: K68-AI1\t");
+        System.out.print("Username: nctrinh\t");
+        System.out.println("Địa chỉ email: nctrinh2705@gmail.com");
+        for(int i = 9; i >= 1; i--){
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.print("No more bottles of beer on the wall.");
+    }
+}
